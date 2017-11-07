05aefb671e253d9bdeb86809d99f1f13c4821c4d
Jason Morris <lemnik@gmail.com>
2017-11-07 08:49:33 +0200

    Chapter 4 - Composing User Interfaces

--- Claim/app/src/main/java/com/packtpub/claim/CaptureClaimActivity.java
+++ Claim/app/src/main/java/com/packtpub/claim/CaptureClaimActivity.java
@@ -1,120 +1,121 @@
 package com.packtpub.claim;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.packtpub.claim.ui.DatePickerWrapper;
-import com.packtpub.claim.ui.IconPickerWrapper;
+import android.os.Bundle;
+
+import android.support.design.widget.FloatingActionButton;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.Toolbar;
+
+import android.text.TextUtils;
+
+import android.view.MenuItem;
+
+import android.view.View;
+
+import android.widget.EditText;
+
+import com.packtpub.claim.model.ClaimItem;
+import com.packtpub.claim.widget.DatePickerLayout;
+import com.packtpub.claim.ui.CategoryPickerFragment;
+import com.packtpub.claim.ui.attachments.AttachmentPagerFragment;
 
 public class CaptureClaimActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final int REQUEST_ATTACH_FILE = 1;
-    private static final int REQUEST_ATTACH_PERMISSION = 1001;
+    public static final String EXTRA_CLAIM_ITEM = "com.packtpub.claim.extras.CLAIM_ITEM";
+    private static final String KEY_CLAIM_ITEM = "com.packtpub.claim.ClaimItem";
 
-    private DatePickerWrapper selectedDate;
-    private RadioGroup categories;
+    private EditText description;
+    private EditText amount;
+
+    private DatePickerLayout selectedDate;
+    private CategoryPickerFragment categories;
+    private AttachmentPagerFragment attachments;
+
+    private ClaimItem claimItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_capture_claim);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton attach = (FloatingActionButton) findViewById(R.id.attach);
         attach.setOnClickListener(this);
 
-        selectedDate = new DatePickerWrapper((TextView) findViewById(R.id.date));
+        selectedDate = (DatePickerLayout) findViewById(R.id.date);
 
-        categories = (RadioGroup) findViewById(R.id.categories);
-        categories.setOnCheckedChangeListener(
-                new IconPickerWrapper((TextView) findViewById(R.id.selected_category))
-        );
+        description = (EditText) findViewById(R.id.description);
+        amount = (EditText) findViewById(R.id.amount);
+        selectedDate = (DatePickerLayout) findViewById(R.id.date);
 
-        categories.check(R.id.other);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        attachments = (AttachmentPagerFragment) fragmentManager.findFragmentById(R.id.attachments);
+        categories = (CategoryPickerFragment) fragmentManager.findFragmentById(R.id.categories);
+
+        if (savedInstanceState != null) {
+            claimItem = savedInstanceState.getParcelable(KEY_CLAIM_ITEM);
+        } else if (getIntent().hasExtra(EXTRA_CLAIM_ITEM)) {
+            claimItem = getIntent().getParcelableExtra(EXTRA_CLAIM_ITEM);
+        }
+
+        if (claimItem == null) {
+            claimItem = new ClaimItem();
+        } else {
+            description.setText(claimItem.getDescription());
+            amount.setText(String.format("%f", claimItem.getAmount()));
+            selectedDate.setDate(claimItem.getTimestamp());
+        }
+
+        attachments.setClaimItem(claimItem);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                return false;
+        }
+
+        return true;
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.attach:
-                onAttachClick();
+                attachments.onAttachClick();
                 break;
         }
     }
 
-    public void onAttachClick() {
-        final int permissionStatus = ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-        );
-
-        if (permissionStatus != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    REQUEST_ATTACH_PERMISSION
-            );
-
-            return;
+    void captureClaimItem() {
+        claimItem.setDescription(description.getText().toString());
+        if (!TextUtils.isEmpty(amount.getText())) {
+            claimItem.setAmount(Double.parseDouble(amount.getText().toString()));
         }
-
-        final Intent attach = new Intent(Intent.ACTION_GET_CONTENT)
-                .addCategory(Intent.CATEGORY_OPENABLE)
-                .setType("*/*");
-
-        startActivityForResult(attach, REQUEST_ATTACH_FILE);
+        claimItem.setTimestamp(selectedDate.getDate());
+        claimItem.setCategory(categories.getSelectedCategory());
     }
 
-    @Override
-    public void onRequestPermissionsResult(
-            final int requestCode,
-            final String[] permissions,
-            final int[] grantResults) {
-
-        switch (requestCode) {
-            case REQUEST_ATTACH_PERMISSION:
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    onAttachClick();
-                }
-                break;
-        }
+    protected void onSaveInstanceState(final Bundle outState) {
+        super.onSaveInstanceState(outState);
+        captureClaimItem(); // make sure the ClaimItem is up-to-date
+        outState.putParcelable(KEY_CLAIM_ITEM, claimItem);
     }
 
-    @Override
-    protected void onActivityResult(
-            final int requestCode,
-            final int resultCode,
-            final Intent data) {
-
-        switch (requestCode) {
-            case REQUEST_ATTACH_FILE:
-                onAttachFileResult(resultCode, data);
-                break;
-        }
+    public void finish() {
+        captureClaimItem();
+        setResult(RESULT_OK, new Intent().putExtra(EXTRA_CLAIM_ITEM, claimItem));
+        super.finish();
     }
 
-    public void onAttachFileResult(final int resultCode, final Intent data) {
-        if (resultCode != RESULT_OK || data == null || data.getData() == null) {
-            return;
-        }
-
-        Toast.makeText(this, data.getDataString(), Toast.LENGTH_SHORT).show();
-    }
 }
